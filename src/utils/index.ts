eb941a13c2417bbb97420a3de01ddda28c14efa1
Haroldo <haroldorodsilva@gmail.com>
2023-12-06 13:43:24 -0400

    First commit

---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export * from './minha-classe-utils'
---
+++ src/utils/minha-classe-utils.ts
@@ -0,0 +1,7 @@
+export class MinhaClasseUtils {
+    constructor(private url: string) { }
+
+    obterUrl(): string {
+        return this.url;
+    }
+}
